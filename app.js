770336c383bd91a849e881cac16773b1850eaae9
absark <absark178@gmail.com>
2020-09-15 17:04:47 +0530

    commit

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 app.use((req,res,next)=>{
     res.setHeader("Access-Control-Allow-Origin","*");
-     res.setHeader("Access-Control-Allow-Headers","Origin,X-Requested-With,Accept,Content-Type,Authorization");
+     res.setHeader("Access-Control-Allow-Headers","Origin,X-Requested-With,Accept,Content-Type,Authorization,XMLHttpRequest");
     res.setHeader("Access-Control-Allow-Methods","GET,POST,PATCH,DELETE,OPTIONS");
    next();
 });
